59929194e3e3dd1d0d9aa5d1451ff002e03a8d92
Matt Robinson <git@nerdoftheherd.com>
2015-10-05 18:35:52 +0100

    Use CMake to generate build files
    Switch from native Visual Studio project and solution files to CMake, as
    this can generate build files for different versions of Visual Studio
    and should allow us to easily support other platforms.

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of libfindindent.
- * Copyright © 2014 by the authors - see the AUTHORS file for details.
+ * Copyright © 2014-2015 by the authors - see the AUTHORS file for details.
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General
  * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
@@ -14,7 +14,6 @@
  * <http://www.gnu.org/licenses/>.
  */
 
-#include "stdafx.h"
 #include "../FindIndent.h"
 
 #define BOOST_TEST_MODULE FindIndent test
